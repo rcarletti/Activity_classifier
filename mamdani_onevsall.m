983065bd7a5ad2e39137b598e9931a982e6d7997
rcarletti <carletti.rossella@gmail.com>
2017-11-11 16:16:21 +0100

    Mamdani 1vsAll FIS

--- mamdani_onevsall.m
+++ mamdani_onevsall.m
@@ -10,7 +10,8 @@ mamdani.onevsall{1} = eval_mamdani_onevsall('Mamdani_1vsAll_all_164s.fis', 1, ..
 
 function mamdani = eval_mamdani_onevsall(filename, act, features, features_ds, ...
                                          sensor_id, time_interval)
-
+    global total_features
+                                     
     % Load Mamdani FIS
 
     mamdani.fis = readfis(filename);
@@ -23,9 +24,16 @@ function mamdani = eval_mamdani_onevsall(filename, act, features, features_ds, .
     for a_id = 1:4
         for v_id = 1:(10 * time_interval)
             for f_id = 1:4
+                if sensor_id == 0
+                    feat = mod(features(f_id) - 1, total_features) + 1;
+                    s_id = ceil(features(f_id) / total_features);
+                else
+                    feat = features(f_id);
+                    s_id = sensor_id;
+                end
+                
                 mamdani.inputs(f_id, (a_id - 1) * 10 * time_interval + v_id) = ...
-                    dsgetfeature(features_ds, features(f_id), ...
-                        sensor_id, a_id, v_id, time_interval);
+                   dsgetfeature(features_ds, feat, s_id, a_id, v_id, time_interval);
             end
 
             mamdani.targets(1, (a_id - 1) * 10 * time_interval + v_id) = ...
